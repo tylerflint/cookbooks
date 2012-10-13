ac0d097470ab3bee5559e66333b3578cb7484b3e
Tyler Flint <tylerflint@gmail.com>
2012-10-13 00:01:11 -0600

    reworking ruby recipe to be idempotent and clean

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -1,31 +1,35 @@
 include_recipe "apt"
 include_recipe "build-essential"
 
-ruby_installed_check = "ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) }"
-
 %w( wget zlib1g-dev libssl-dev libffi-dev libxml2-dev libncurses5-dev libreadline6-dev ).each do |pkg|
   package pkg do
     action :install
   end
 end
 
-execute "get & unpack #{ node[:ruby][:version] }" do
-  user "root"
-  command "cd /usr/local/src && wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-#{ node[:ruby][:version] }.tar.bz2 && tar xjf ruby-#{ node[:ruby][:version] }.tar.bz2 && cd ruby-#{ node[:ruby][:version] }"
-  # not_if ruby_installed_check
+execute "get #{ node[:ruby][:version] }" do
+  cwd "/usr/local/src"
+  command "wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-#{ node[:ruby][:version] }.tar.bz2"
+  creates "/usr/local/src/ruby-#{ node[:ruby][:version] }.tar.bz2"
+end
+
+execute "unpack #{ node[:ruby][:version] }" do
+  cwd '/usr/local/src'
+  command "tar xjf ruby-#{ node[:ruby][:version] }.tar.bz2"
+  creates "/usr/local/src/ruby-#{ node[:ruby][:version] }" }
 end
 
 execute "configure & make #{ node[:ruby][:version] }" do
   user "root"
   command "cd /usr/local/src/ruby-#{ node[:ruby][:version] } && ./configure && make && make install"
-  # not_if ruby_installed_check
+  not_if { `ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) } | grep wc -l`.to_i != 0 }
 end
 
 %w( openssl readline ).each do |ext|
   execute "configure & make #{ node[:ruby][:version] } #{ext} support" do
     user "root"
     command "cd /usr/local/src/ruby-#{ node[:ruby][:version] }/ext/#{ext}/ && ruby extconf.rb && make && make install"
-    # not_if ruby_installed_check
+    not_if { `ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) } | grep wc -l`.to_i != 0 }
   end
 end
 
