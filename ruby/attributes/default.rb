04625fcfc8a09c9b2b0a725977d0e9029167f51c
Tyler Flint <tylerflint@gmail.com>
2012-10-13 11:36:13 -0600

    modifying the ruby cookbook to include all dependencies

--- ruby/attributes/default.rb
+++ ruby/attributes/default.rb
@@ -1,1 +1,2 @@
 default[:ruby][:version] = "1.9.3-p194"
+default[:ruby][:deps] = %w( openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config )
--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -1,32 +1,32 @@
 include_recipe "apt"
 include_recipe "build-essential"
 
-%w( wget zlib1g-dev libssl-dev libffi-dev libxml2-dev libncurses5-dev libreadline6-dev ).each do |pkg|
+node[:ruby][:deps].each do |pkg|
   package pkg do
     action :install
   end
 end
 
-execute "get #{ node[:ruby][:version] }" do
+execute "get ruby-#{ node[:ruby][:version] }" do
   cwd "/usr/local/src"
   command "wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-#{ node[:ruby][:version] }.tar.bz2"
   creates "/usr/local/src/ruby-#{ node[:ruby][:version] }.tar.bz2"
 end
 
-execute "unpack #{ node[:ruby][:version] }" do
+execute "unpack ruby-#{ node[:ruby][:version] }" do
   cwd '/usr/local/src'
   command "tar xjf ruby-#{ node[:ruby][:version] }.tar.bz2"
   creates "/usr/local/src/ruby-#{ node[:ruby][:version] }"
 end
 
-execute "configure & make #{ node[:ruby][:version] }" do
+execute "build ruby-#{ node[:ruby][:version] }" do
   cwd "/usr/local/src/ruby-#{ node[:ruby][:version] }"
   command "./configure && make && make install"
   not_if { `ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) } | wc -l`.to_i != 0 }
 end
 
 %w( openssl readline ).each do |ext|
-  execute "configure & make #{ node[:ruby][:version] } #{ext} support" do
+  execute "configure & make ruby-#{ node[:ruby][:version] } #{ext} support" do
     cwd "/usr/local/src/ruby-#{ node[:ruby][:version] }/ext/#{ext}"
     command "ruby extconf.rb && make && make install"
     not_if { `ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) } | wc -l`.to_i != 0 }
